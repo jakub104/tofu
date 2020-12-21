c8b91ec6ec827a474d72c4ef745707ad1c84bd35
jakub104 <jakubzlezena@wp.pl>
2020-12-21 15:28:17 +0100

    deploy

---
+++ precache-manifest.fcc24bb01ea48a4b525c61e306bc0be5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630a80d97fbab175ae8f",
+    "url": "/tofu/css/app.630a571e.css"
+  },
+  {
+    "revision": "bbbd5d3b86488164d9ec",
+    "url": "/tofu/css/chunk-0cbaae53.e93c3b9a.css"
+  },
+  {
+    "revision": "f29469b6d993a39fb932",
+    "url": "/tofu/css/chunk-12629358.a6c69b18.css"
+  },
+  {
+    "revision": "adb1d8d2d678a8396179",
+    "url": "/tofu/css/chunk-7309fb2e.a780dc67.css"
+  },
+  {
+    "revision": "46aacd092f82c6a310b1",
+    "url": "/tofu/css/chunk-89b66538.1628fe8e.css"
+  },
+  {
+    "revision": "af9c8f74518d3eba7fab",
+    "url": "/tofu/css/chunk-ce2d957c.8f2751cd.css"
+  },
+  {
+    "revision": "dcb588cfff2bf11d0777",
+    "url": "/tofu/css/chunk-e6fb361a.324227b5.css"
+  },
+  {
+    "revision": "08c7865d7e116fc450a0653ccb38e0fb",
+    "url": "/tofu/img/logo.08c7865d.svg"
+  },
+  {
+    "revision": "232457d0c3f8749275d7d74ae1d2c5a4",
+    "url": "/tofu/img/logotype.232457d0.svg"
+  },
+  {
+    "revision": "c38468e9e93267e355399617c48bfa7d",
+    "url": "/tofu/index.html"
+  },
+  {
+    "revision": "630a80d97fbab175ae8f",
+    "url": "/tofu/js/app.13a178ea.js"
+  },
+  {
+    "revision": "bbbd5d3b86488164d9ec",
+    "url": "/tofu/js/chunk-0cbaae53.b04d41b0.js"
+  },
+  {
+    "revision": "f29469b6d993a39fb932",
+    "url": "/tofu/js/chunk-12629358.fe1d9d02.js"
+  },
+  {
+    "revision": "adb1d8d2d678a8396179",
+    "url": "/tofu/js/chunk-7309fb2e.786c706e.js"
+  },
+  {
+    "revision": "46aacd092f82c6a310b1",
+    "url": "/tofu/js/chunk-89b66538.264bbca5.js"
+  },
+  {
+    "revision": "af9c8f74518d3eba7fab",
+    "url": "/tofu/js/chunk-ce2d957c.81120c51.js"
+  },
+  {
+    "revision": "dcb588cfff2bf11d0777",
+    "url": "/tofu/js/chunk-e6fb361a.22884b89.js"
+  },
+  {
+    "revision": "3abb206f1a90dbac8b2c",
+    "url": "/tofu/js/chunk-vendors.4adc2588.js"
+  },
+  {
+    "revision": "feae235f42935efe63099b9efd0e685c",
+    "url": "/tofu/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tofu/robots.txt"
+  }
+]);
